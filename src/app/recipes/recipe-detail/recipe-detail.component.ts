b557dc8b9a2c91da431c915ad4c64cceb6971576
Arya TS <aryasreekala1997@gmail.com>
2023-02-17 12:49:33 +0530

    Adding the Shopping List Service, Using Services for Pushing Data from A to B,Adding Ingredients to Recipes,Passing Ingredients from Recipes to the Shopping List (via a Service)

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit,Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -7,10 +8,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input() recipe:Recipe;
-  constructor(){
+  constructor(private recipeService:RecipeService){
 
   }
   ngOnInit(){
+
+  }
+  onAddToShoppingList(){
+    this.recipeService.addingIngredientsToShoppingList(this.recipe.ingredients);
 
   }
 
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,39 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter ,Injectable} from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list-service";
 import { Recipe } from "./recipe.model";
+
+@Injectable()
 export class RecipeService{
     recipeSelected=new EventEmitter<Recipe>();
      
     private recipes:Recipe[] = [
-        new Recipe('Pasta', 'Pasta casserole with tomatoes and mozzarella cheese in a cast iron pan on a light grey slate, stone or concrete background.','https://media.istockphoto.com/id/1190036539/photo/pasta-casserole-with-tomatoes-and-mozzarella-cheese-in-a-cast-iron-pan.jpg?b=1&s=170667a&w=0&k=20&c=iPbpz032rVCcSLZ3VEk3vE27XTr1B46cL2l3TTWxHJ4='), 
-        new Recipe('Cherry Cream Cheese Dessert', 'Pretty layers of graham cracker crumbs, tasty filling and fruit topping make these cream cheese desserts a standout! For a nice change, you can substitute blueberry pie filling or another fruit flavor for the filling called for in the recipe.','https://www.tasteofhome.com/wp-content/uploads/2018/01/exps21585_THCA153054D10_15_4b.jpg') 
+        new Recipe('Pasta', 'Pasta casserole with tomatoes and mozzarella cheese in a cast iron pan on a light grey slate, stone or concrete background.',
+        'https://media.istockphoto.com/id/1190036539/photo/pasta-casserole-with-tomatoes-and-mozzarella-cheese-in-a-cast-iron-pan.jpg?b=1&s=170667a&w=0&k=20&c=iPbpz032rVCcSLZ3VEk3vE27XTr1B46cL2l3TTWxHJ4=', 
+        [
+          new Ingredient('Pasta',1),
+          new Ingredient('Onion',1),
+          new Ingredient('Tomatos',1),
+        ]),
+        new Recipe('Cherry Cream Cheese Dessert', 
+        'Pretty layers of graham cracker crumbs, tasty filling and fruit topping make these cream cheese desserts a standout! For a nice change, you can substitute blueberry pie filling or another fruit flavor for the filling called for in the recipe.',
+        'https://www.tasteofhome.com/wp-content/uploads/2018/01/exps21585_THCA153054D10_15_4b.jpg',
+        [
+          new Ingredient('Cup crushed shortbread cookies ',1),
+          new Ingredient('Unsalted butter',3),
+          new Ingredient('Sugar',2)
+         
+        ])
       ];
+      constructor(private slService:ShoppingListService){
+
+      }
     
       getRecipes(){
         return this.recipes.slice();
+      }
+      addingIngredientsToShoppingList(ingredients:Ingredient[]){
+        this.slService.addIngredients(ingredients);
       }
 
 }
